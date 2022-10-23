c5db58953889afbe8c1eab5507662dc4056a840b
Landrewvr <luis.andrew.veras@gmail.com>
2022-10-23 13:41:51 -0400

    fixing prod route

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   app_port: 4400,
-  API_URL: 'https://mflix-backend-lvr.netlify.app/.netlify/functions/movies'
+  API_URL: 'https://mflix-backend-lvr.netlify.app/.netlify/functions/movies/'
 };
